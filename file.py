9a7569c2381ee9625a3c6e129b280a869dfddf3a
Abdoulaye BA <abdoulaye21.ba@ucad.edu.sn>
2024-09-05 22:32:50 +0000

    first commit

---
+++ file.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Lire les données à partir d'un fichier exel
+data = pd.read_excel('CLIM_DAKAR(2009-2023).xlsx')
+print(data)
+
+# Créer un graphique
+plt.figure(figsize=(10, 5))
+plt.plot(data, marker='o')
+plt.title('Température Moyenne par Année')
+plt.xlabel('Année')
+plt.ylabel('Température Moyenne (°C)')
+plt.grid(True)
+plt.show()
